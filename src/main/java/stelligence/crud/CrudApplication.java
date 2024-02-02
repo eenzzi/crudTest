454baf7471abe2a28c56f87c25c68c94dbdcb1e9
eenzzi <purelej99@gmail.com>
2024-02-02 17:37:37 +0900

    refactor: redis 설정

--- src/main/java/stelligence/crud/CrudApplication.java
+++ src/main/java/stelligence/crud/CrudApplication.java
@@ -2,10 +2,12 @@ package stelligence.crud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableCaching
 public class CrudApplication {
 
 	public static void main(String[] args) {
--- src/test/java/stelligence/crud/CrudTest.java
+++ src/test/java/stelligence/crud/CrudTest.java
@@ -1,69 +1,63 @@
 package stelligence.crud;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import stelligence.crud.entity.Post;
+
 import stelligence.crud.repository.PostRepository;
-
-import java.time.LocalDateTime;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 public class CrudTest {
 
-    @Autowired
-    PostRepository postRepository;
+	@Autowired
+	PostRepository postRepository;
 
-    @Test
-    public void test() {
+	// @Test
+	// public void test() {
+	//
+	//     Post newPost = new Post(null, "테스트 제목", "테스트 내용", false);
+	//
+	//     // Post 저장
+	//     Post savedPost = postRepository.save(newPost);
+	//
+	//     // 검증
+	//     assertNotNull(savedPost.getId());
+	//     assertEquals("테스트 제목", savedPost.getTitle());
+	//     assertEquals("테스트 내용", savedPost.getContent());
+	//
+	// }
 
-        Post newPost = new Post(null, "테스트 제목", "테스트 내용", false);
-
-        // Post 저장
-        Post savedPost = postRepository.save(newPost);
-
-        // 검증
-        assertNotNull(savedPost.getId());
-        assertEquals("테스트 제목", savedPost.getTitle());
-        assertEquals("테스트 내용", savedPost.getContent());
-
-    }
-
-//    @Test
-//    public void find() {
-////        List<Member> all = memberRepository.findAll();
-////
-////        for (Member member : all) {
-////            System.out.println("member = " + member);
-////        }
-//
-//        Member member = memberRepository.findById(1L).orElseThrow(
-//                () -> new IllegalArgumentException("해당 유저가 없습니다.")
-//        );
-//
-//        System.out.println("member = " + member);
-//    }
-//
-//    @Test
-//    public void delete() {
-//
-//        memberRepository.deleteById(1L);
-//    }
-//
-//    @Test
-//    public void update() {
-//        Member member = memberRepository.findById(2L)
-//                .orElseThrow(() -> new IllegalArgumentException("해당 유저가 없습니다."));
-//
-//        int beforeAge = member.getAge();
-//        System.out.println("beforeAge = " + beforeAge);
-//
-//        member.setAge(member.getAge() + 1);
-//
-//        int afterAge = member.getAge();
-//        System.out.println("afterAge = " + afterAge);
-//    }
+	//    @Test
+	//    public void find() {
+	////        List<Member> all = memberRepository.findAll();
+	////
+	////        for (Member member : all) {
+	////            System.out.println("member = " + member);
+	////        }
+	//
+	//        Member member = memberRepository.findById(1L).orElseThrow(
+	//                () -> new IllegalArgumentException("해당 유저가 없습니다.")
+	//        );
+	//
+	//        System.out.println("member = " + member);
+	//    }
+	//
+	//    @Test
+	//    public void delete() {
+	//
+	//        memberRepository.deleteById(1L);
+	//    }
+	//
+	//    @Test
+	//    public void update() {
+	//        Member member = memberRepository.findById(2L)
+	//                .orElseThrow(() -> new IllegalArgumentException("해당 유저가 없습니다."));
+	//
+	//        int beforeAge = member.getAge();
+	//        System.out.println("beforeAge = " + beforeAge);
+	//
+	//        member.setAge(member.getAge() + 1);
+	//
+	//        int afterAge = member.getAge();
+	//        System.out.println("afterAge = " + afterAge);
+	//    }
 }
